a3b7ec3ff3b9dede485784c2dfe20c43c2fb41a3
njoroge015 <costetechdynamics@gmail.com>
2026-08-15 16:15:15 +0300

    Revert: restore PowerGrid vendor import in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import flatpickr from "flatpickr";
 import TomSelect from "tom-select";
 import "./bootstrap";
+import "./../../vendor/power-components/livewire-powergrid/dist/powergrid";
 
 // @ts-ignore
 window.TomSelect = TomSelect;
