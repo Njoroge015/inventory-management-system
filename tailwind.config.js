4c91035689e89466a0069bec2020ff96901f0097
njoroge015 <costetechdynamics@gmail.com>
2026-08-15 15:34:30 +0300

    Fix tailwind config to handle missing vendor directory in production builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
+const presets = [];
+try {
+    presets.push(require("./vendor/power-components/livewire-powergrid/tailwind.config.js"));
+} catch (e) {
+    // vendor directory not available (e.g., in production builds)
+}
+
 export default {
-    presets: [
-        require("./vendor/power-components/livewire-powergrid/tailwind.config.js"),
-    ],
+    presets: presets,
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/views/livewire/**/*.blade.php",
         "./app/Livewire/**/*Table.php",
-        "./vendor/power-components/livewire-powergrid/resources/views/**/*.php",
-        "./vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php",
         "./app/Enums/**/*.php",
     ],
 
