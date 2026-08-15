45b1c05861b2f3d4c316c0c58d3898f3fc4ebd20
njoroge015 <costetechdynamics@gmail.com>
2026-08-15 15:24:05 +0300

    changes for deployment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import flatpickr from "flatpickr";
 import TomSelect from "tom-select";
 import "./bootstrap";
-import "./../../vendor/power-components/livewire-powergrid/dist/powergrid";
 
 // @ts-ignore
 window.TomSelect = TomSelect;
