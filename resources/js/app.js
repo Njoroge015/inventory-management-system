64da19df48055177ec81c171a0a45d03d2f90377
njoroge015 <costetechdynamics@gmail.com>
2026-08-15 16:33:50 +0300

    Netlify: run composer before build; annotate PowerGrid import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import flatpickr from "flatpickr";
 import TomSelect from "tom-select";
 import "./bootstrap";
+// PowerGrid vendor import: required when Composer packages are available during build
+// If your CI doesn't run `composer install`, comment this line to avoid build errors.
 import "./../../vendor/power-components/livewire-powergrid/dist/powergrid";
 
 // @ts-ignore
